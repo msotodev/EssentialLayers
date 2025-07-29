a788f1e1445059cbdc1b6fe9bc6e12139ae2460b
Mario <mariosotomor.dev@gmail.com>
2025-07-29 14:35:29 -0600

    New override to Success in ResultHelper + Error messages has been removed `29/07/2025`

--- EssentialLayers/Helpers/Result/ResultHelper.cs
+++ EssentialLayers/Helpers/Result/ResultHelper.cs
@@ -1,45 +1,23 @@
-﻿using EssentialLayers.Helpers.Extension;
-using Newtonsoft.Json;
-using System;
-using System.Runtime.CompilerServices;
+﻿using System;
 
 namespace EssentialLayers.Helpers.Result
 {
 	public class ResultHelper<T>(bool ok, string message, T data)
 	{
-		public bool Ok { get; set; } = ok;
+		public T Data { get; set; } = data;
 
 		public string Message { get; set; } = message;
 
-		public T Data { get; set; } = data;
+		public bool Ok { get; set; } = ok;
 
 		/**/
 
-		public static ResultHelper<T> Success(T data) => new(true, string.Empty, data);
-
 		public static ResultHelper<T> Fail(string message) => new(false, message, default!);
 
-		public static ResultHelper<T> Fail(
-			Exception e,
-			[CallerFilePath] string file = null,
-			[CallerMemberName] string member = null,
-			[CallerLineNumber] int lineNumber = 0
-		)
-		{
-			if (e.InnerException is JsonSerializationException jsonEx) return Fail(
-				$"Error de deserialización en la ruta '{jsonEx.Path}': {jsonEx.Message}"
-			);
+		public static ResultHelper<T> Fail(Exception e) => Fail(e.Message);
 
-			Type type = e.InnerException?.GetType();
+		public static ResultHelper<T> Success(T data) => new(true, string.Empty, data);
 
-			if (type != null && ErrorMessages.Messages.TryGetValue(type, out string message) && message.NotNull())
-			{
-				return new ResultHelper<T>(
-					false, message, default!
-				);
-			}
-
-			return Fail(e.Message);
-		}
+		public static ResultHelper<T> Success(T data, string message) => new(true, message, data);
 	}
 }
