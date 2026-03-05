a7597d17d58e16a437ffc9a60a8da75ed3471e4f
msotodev <mariosotomor.dev@gmail.com>
2026-03-04 21:06:48 -0600

    refactor: apply SRP by splitting DapperExtension and ConnectionHelper into focused classes

---
+++ EssentialLayers.Dapper/Builders/DataTableBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+namespace EssentialLayers.Dapper.Builders
+{
+	public static class DataTableBuilder
+	{
+		public static DataTable Build<T>(this IEnumerable<T> source)
+		{
+			if (source == null) return new DataTable();
+
+			using DataTable result = new();
+
+			PropertyInfo[] properties = source.FirstOrDefault()!.GetType().GetProperties();
+
+			foreach (PropertyInfo property in properties)
+			{
+				result.Columns.Add(property.Name, property.PropertyType);
+			}
+
+			foreach (T item in source)
+			{
+				DataRow dataRow = result.NewRow();
+				PropertyInfo[] itemProperties = item!.GetType().GetProperties();
+
+				foreach (PropertyInfo property in itemProperties)
+				{
+					dataRow[property.Name] = property.GetValue(item)!;
+				}
+
+				result.Rows.Add(dataRow);
+			}
+
+			return result;
+		}
+	}
+}
---
+++ EssentialLayers.Dapper/Parsers/DynamicParametersParser.cs
@@ -0,0 +1,56 @@
+﻿using Dapper;
+using EssentialLayers.Dapper.Builders;
+using EssentialLayers.Dapper.Mappers;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+
+namespace EssentialLayers.Dapper.Parsers
+{
+	public static class DynamicParametersParser
+	{
+		public static DynamicParameters Parse<T>(this T source)
+		{
+			if (source == null) return new DynamicParameters();
+
+			List<PropertyInfo> properties = [.. source.GetType().GetProperties()];
+			DynamicParameters dynamicParameters = new();
+
+			properties.ForEach(property =>
+			{
+				object value = property.GetValue(source)!;
+				string parameterName = $"@{property.Name}";
+
+				if (property.PropertyType.IsValueType)
+				{
+					DbType dbType = property.PropertyType.ToDbType();
+
+					dynamicParameters.Add(parameterName, value, dbType);
+				}
+				else if (
+					property.PropertyType.IsGenericType &&
+					property.PropertyType.GetGenericTypeDefinition() == typeof(List<>)
+				)
+				{
+					IEnumerable<object> enumerable = (value as IEnumerable<object>)!;
+					DataTable dataTable = enumerable.Build();
+
+					dynamicParameters.Add(parameterName, dataTable);
+				}
+				else if (property.PropertyType.IsClass)
+				{
+					List<PropertyInfo> nestedProperties = [.. value.GetType().GetProperties()];
+
+					nestedProperties.ForEach(nestedProperty =>
+					{
+						DbType dbType = nestedProperty.PropertyType.ToDbType();
+
+						dynamicParameters.Add(parameterName, nestedProperty.GetValue(value), dbType);
+					});
+				}
+			});
+
+			return dynamicParameters;
+		}
+	}
+}
