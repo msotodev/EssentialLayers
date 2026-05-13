ac07a8172376cf48889a6142c4d0dec3e65c2110
Mario <mariosotomor.dev@gmail.com>
2026-05-13 17:17:53 -0600

    feat: Support to patch method with 3 overrides in IHttpFactory `13-05-2026`

---
+++ EssentialLayers.Request/Models/FileRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace EssentialLayers.Request.Models
+{
+	public class FileRequest
+	{
+		public string Name { get; set; } = string.Empty;
+
+		public string FileName { get; set; } = string.Empty;
+
+		public Stream Stream { get; set; } = Stream.Null;
+	}
+}
--- EssentialLayers.Request/Services/Factory/IHttpFactory.cs
+++ EssentialLayers.Request/Services/Factory/IHttpFactory.cs
@@ -1,4 +1,7 @@
-﻿using EssentialLayers.Request.Helpers;
+﻿using EssentialLayers.Helpers.Result;
+using EssentialLayers.Request.Helpers;
+using EssentialLayers.Request.Models;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -12,7 +15,19 @@ namespace EssentialLayers.Request.Services.Factory
 
 		Task<HttpResponse<byte[]>> GetBytesAsync(string clientName, string url);
 
-		Task<HttpResponse<TResult>> PostAsync<TResult, TRequest>(string clientName, string url, TRequest request);
+		Task<HttpResponse<TResult>> PostAsync<TResult, TRequest>(
+			string clientName, string url, TRequest request
+		);
+
+		Task<Response> PostAsync(
+			string clientName, string url, FileRequest fileRequest, IDictionary<string, string>? headers = default
+		);
+
+		Task<HttpResponse<TResult>> PatchAsync<TResult, TRequest>(string clientName, string url, TRequest request);
+
+		Task<HttpResponse<TResult>> PatchAsync<TResult>(string clientName, string url);
+
+		Task<Response> PatchAsync(string clientName, string url);
 
 		Task<HttpResponse<TResult>> PutAsync<TResult, TRequest>(string clientName, string url, TRequest request);
 
