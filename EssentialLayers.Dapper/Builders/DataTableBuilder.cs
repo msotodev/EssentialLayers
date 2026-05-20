c2fd9467a9d6c0a6bbc9f0e849f6466b95c4e704
Mario <mariosotomor.dev@gmail.com>
2026-05-20 10:06:24 -0600

    feat: Nullable support caused by error "DataSet does not support System.Nullable<>"

--- EssentialLayers.Dapper/Builders/DataTableBuilder.cs
+++ EssentialLayers.Dapper/Builders/DataTableBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using EssentialLayers.Dapper.Cache;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -8,17 +9,21 @@ namespace EssentialLayers.Dapper.Builders
 {
 	internal static class DataTableBuilder
 	{
+		private static Type GetColumnType(Type type)
+		{
+			return Nullable.GetUnderlyingType(type) ?? type;
+		}
+
 		internal static DataTable Build<T>(this IEnumerable<T> source)
 		{
 			if (source == null) return new DataTable();
 
 			using DataTable result = new();
-
 			PropertyInfo[] properties = source.ToList().First().GetCachedProperties();
 
 			foreach (PropertyInfo property in properties)
 			{
-				result.Columns.Add(property.Name, property.PropertyType);
+				result.Columns.Add(property.Name, GetColumnType(property.PropertyType));
 			}
 
 			foreach (T item in source)
@@ -27,7 +32,7 @@ namespace EssentialLayers.Dapper.Builders
 
 				foreach (PropertyInfo property in properties)
 				{
-					dataRow[property.Name] = property.GetValue(item)!;
+					dataRow[property.Name] = property.GetValue(item) ?? DBNull.Value;
 				}
 
 				result.Rows.Add(dataRow);
@@ -41,19 +46,18 @@ namespace EssentialLayers.Dapper.Builders
 			if (source == null) return new DataTable();
 
 			using DataTable result = new();
-
 			PropertyInfo[] properties = source.GetCachedProperties();
 
 			foreach (PropertyInfo property in properties)
 			{
-				result.Columns.Add(property.Name, property.PropertyType);
+				result.Columns.Add(property.Name, GetColumnType(property.PropertyType));
 			}
 
 			DataRow dataRow = result.NewRow();
 
 			foreach (PropertyInfo property in properties)
 			{
-				dataRow[property.Name] = property.GetValue(source)!;
+				dataRow[property.Name] = property.GetValue(source) ?? DBNull.Value;
 			}
 
 			result.Rows.Add(dataRow);
